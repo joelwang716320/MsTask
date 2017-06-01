e2778894647b16bc2223105cfed8ecfefcf3ccc7
joelww <15183691117@163.com>
2017-06-01 17:40:24 +0800

    Update the way to submit comments

--- testCases/liveCommentary.py
+++ testCases/liveCommentary.py
@@ -1,28 +1,60 @@
 __author__ = 'Administrator'
 
+import random
 import requests
 from baseData import loginResponseData
 from baseData import requestsData
+from testCases import putQuestions
 
+SELLERID = []
+OBJECT_ID = []
+KEY = loginResponseData.key
+MEMBER_ID = loginResponseData.member_id
 def liveComment():
     """
     Live Room review
     """
-    object_id = 7103
-    tape = 3
-    message = 'test for live comment'
-    pid = 0
-    appId = 'android'
-    key = loginResponseData.key
-    member_id = loginResponseData.member_id
-    liveComData = {'object_id': object_id,
-                   'type': tape,
-                   'message': message,
-                   'pid': pid,
-                   'appId': appId,
-                   'key': key,
-                   'member_id': member_id,}
-    liveComUrl = requestsData.liveCommentUrl
-    reLiveCom = requests.post(url = liveComUrl,data = liveComData,verify = False)
 
-    print "askStock :=====>%s"%reLiveCom.json()
+    message = ['test for live comment oh','teseteye','sdfgsdfgsege']
+    for i in range(0,len(message)-1):
+        getLiveComment() # init
+        liveComData = {'object_id': OBJECT_ID,
+                       'type': 3,
+                       'message': message[i],
+                       'pid': 0,
+                       'appId': 'android',
+                       'key': KEY,
+                       'member_id': MEMBER_ID}
+        liveComUrl = requestsData.liveCommentUrl
+        reLiveCom = requests.post(url = liveComUrl,data = liveComData,verify = False)
+
+        print "askStock :=====>%s"%reLiveCom.json()
+        print OBJECT_ID
+
+
+def getLiveComment():
+    """
+    First, get to the teacher ID and then get the teacher's dynamic(PolicyID)
+    """
+    global SELLERID
+    global OBJECT_ID
+    putQuestions.getLiveStockInfo()
+    SELLERID = random.sample(putQuestions.SELLER_ID,1)
+
+    policyData = {'seller_id': SELLERID,
+                  'is_history_point': 0,
+                  'refresh': 0,
+                  'pagesize': 1,  # Show the latest one
+                  'appId': 'android',
+                  'key': KEY,
+                  'member_id': MEMBER_ID}
+
+    liveCimmentArrayUrl = requestsData.liveCimmentArrayUrl
+    getResponData = requests.get(url = liveCimmentArrayUrl,params = policyData,verify = False)
+    reData = getResponData.json()
+
+    policyID = reData['data']['policy_list'][0]['policy_id']
+    OBJECT_ID = policyID
+
+
+liveComment()
