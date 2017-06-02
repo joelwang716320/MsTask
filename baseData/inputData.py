42f83b987e5fbbaaaf4e9e8da4e6cdb2bda793f1
joelww <15183691117@163.com>
2017-06-02 12:53:34 +0800

    Move the input data to the file

--- baseData/inputData.py
+++ baseData/inputData.py
@@ -1,7 +1,12 @@
 __author__ = 'Administrator'
 
 #input data
+putQuestions = 'this is a questions'
+#
 liveCommentMessage = ['review_1','review_2','review_3']
+# ask stock
+askStockQuestions = 'ask stock'
+stock_code = 'sz000725'
 
 loginData = {'password': '123456',
              'username': '15183691117',
