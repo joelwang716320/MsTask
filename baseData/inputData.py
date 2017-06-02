610120f995bea77c382ebd856e02e7b5ae254638
joelww <15183691117@163.com>
2017-06-02 09:52:33 +0800

    get array of seller_id and Remove an SELLER_ID from the array randomly

--- baseData/inputData.py
+++ baseData/inputData.py
@@ -2,3 +2,7 @@ __author__ = 'Administrator'
 
 #input data
 liveCommentMessage = ['review_1','review_2','review_3']
+
+loginData = {'password': '123456',
+             'username': '15183691117',
+             'verify': 'False'}
