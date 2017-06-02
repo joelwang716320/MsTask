710ee8d067bf31db43055f0aafc4773c4e6bc14f
joelww <15183691117@163.com>
2017-06-02 09:48:46 +0800

    add stock machine URL

--- baseData/requestsData.py
+++ baseData/requestsData.py
@@ -5,8 +5,17 @@ loginUrl = 'https://login.api.guxiansheng.cn/index.php?c=user&a=login'
 askStockUrl = 'https://u.api.guxiansheng.cn/index.php?c=question&a=post'
 putQuestionUrl = 'https://clb.api.guxiansheng.cn/index.php?c=policy&a=questionsPost&v=1.1'
 liveCommentUrl = 'https://u.api.guxiansheng.cn/index.php?c=comment&a=post&v=1.1'
+#live array url
 liveStockUrl = 'https://clb.api.guxiansheng.cn/index.php?c=policy&a=policySellers&v=2.0.1'
+#live comment array url
+liveCimmentArrayUrl= 'https://clb.api.guxiansheng.cn/index.php?c=policy&a=getPolicyList&v=2.0.1'
 
+#sign
+signUrl = 'https://u.api.guxiansheng.cn/index.php?c=sign&a=check'
+#Stock machine
+getObjectID = 'https://clb.api.guxiansheng.cn/index.php?c=plan&a=today_list&v=2.0.1' # curpage=1&pagesize=200&appId=android&key=028c3042883381070b379b643a07dd12&member_id=1131327424
+getHashKey = 'https://trade.api.guxiansheng.cn/index.php?c=buy&a=get&v=1.1' # object_id=85&goods_type=1&appId=android&key=028c3042883381070b379b643a07dd12&member_id=1131327424
+stockMachine = 'https://trade.api.guxiansheng.cn/index.php?c=buy&a=nb_step2'  # goods_id=1&hashkey=1131327424_5930bbb7bd1a8&channel=xiaomi&appId=android&key=028c3042883381070b379b643a07dd12&member_id=1131327424
 #data
 loginData = {'password': '123456',
              'username': '15183691117',
