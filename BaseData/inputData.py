38ea3bcb9d16b0ec962f8401c07b2cc39a180046
IDO.W <15183691117@163.com>
2017-06-04 23:36:09 +0800

    update input data

--- BaseData/inputData.py
+++ BaseData/inputData.py
@@ -1,13 +1,16 @@
+#coding=utf-8
 __author__ = 'Administrator'
-
-#input data
-putQuestions = 'this is a questions'
-#
-liveCommentMessage = ['review_11','review_22','review_33']
 # ask stock
-askStockQuestions = 'ask stock'
-stock_code = 'sz000725'
-
-loginData = {'password': 'shmily',
-             'username': '15196685175',
+askStockQuestions = '老师您好！近期这只股票怎么样'
+stock_code = ['sz000725', 'sz300658', 'sz300660', 'sh603042', 'sh603042', 'sz300217', 'sz000667', 'sz000573', 'sz002549',
+              'sz300255', 'sz000558', 'sz300655', 'sz300447', 'sz000553', 'sz300479', 'sz002875', 'sz002836', 'sz002809']
+# login App data
+loginData = {'password': '123456',
+             'username': '17683240598',
              'verify': 'False'}
+
+# Ask the teacher questions about the Live Room
+putQuestions = "这是向老师提问的内容"
+# Live Room comment content
+commentContent = ["老师,早上好!","老师我经常看你的节目，您觉得最近会有哪些方面的调整","老师呀 我可经常看你的直播"
+                  "O(∩_∩)O哈哈~","期待！","您的直播太经常","谢谢老师","什么时候有镜囊","咋整","good","oh~~~~"]
